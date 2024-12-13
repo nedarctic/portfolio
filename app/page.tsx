b9d67afb75351ab7517eae73fe25ded73cd60be8
nedarctic <nedarctic@gmail.com>
2024-12-13 22:39:29 +0300

    Added changes to the UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { AboutText } from "./components/abouttext";
 import { CoverText } from "./components/covertext";
@@ -23,11 +24,12 @@ export default function Home() {
         <Image src="/aboutimage.png" alt="Justus Kimtai about image" width={300} height={340} className={`p-4`} />
       </div>
       <div className="flex flex-col justify-center items-center py-5 space-y-10">
-        <p className={`${bodoniModa.className} ${styles.scrollanimate} font-black italic text-2xl`}>Work</p>
-        <p className={`${bodoniModa.className} ${styles.scrollanimate} font-black self-start italic px-4 text-4xl`}>GalleryX</p>
+      <p className={`${bodoniModa.className} ${styles.scrollanimate} font-black italic px-4 text-6xl`}>Work</p>
+        <p className={`${bodoniModa.className} ${styles.scrollanimate} font-black italic text-4xl`}>GalleryX</p>
         <p className={`${bodoniModa.className} ${styles.scrollanimate} italic text-black text-sm px-4`}>A modern web application for artists, photographers, and creators to display their portfolios and collections in an elegant, user-friendly interface.</p>
-        <Image src="/gallx.png" alt="Gallery X app demo" width={300} height={0} className={`${styles.scrollanimate}`} />
-        <p className={`${bodoniModa.className} ${styles.scrollanimate} text-black italic text-2xl`}>More coming soon!</p>
+        <Link href="https://galleryx.onrender.com">
+          <Image src="/gallx.png" alt="Gallery X app demo" width={300} height={0} className={`${styles.scrollanimate} px-4`} />
+        </Link><p className={`${bodoniModa.className} ${styles.scrollanimate} text-black italic text-2xl`}>More coming soon!</p>
       </div>
       <div className="flex flex-col justify-end items-center p-10">
         <ThankYou />
