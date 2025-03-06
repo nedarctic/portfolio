2b4e9d3edc523b8f570d123cd223efaab207208c
nedarctic <nedarctic@gmail.com>
2025-03-06 22:44:20 +0300

    Fixed footer font

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,13 +12,13 @@ export const Footer = () => {
                         <Link href="https://wa.me/+254797517270" target='_blank' rel='noopener noreferrer' className="block">
                             <div className='flex items-center flex-row space-x-3'>
                                 <Image src="/whatsappicon.svg" alt="WhatsApp icon" width={40} height={40} />
-                                <p className={`${spaceMono.className} font-bodoni text-black font-normal text-sm lg:text-lg md:text-md space-x-5`}>+254797517270</p>
+                                <p className={`${spaceMono.className} text-black font-normal text-sm lg:text-lg md:text-md space-x-5`}>+254797517270</p>
                             </div>
                         </Link>
                         <Link href="mailto:justkimtai@gmail.com" target="_blank" rel="noopener noreferrer" className='block'>
                             <div className='flex items-center flex-row space-x-3'>
                                 <Image src="/gmailicon.svg" alt="Gmail icon" width={40} height={40} />
-                                <p className={`${spaceMono.className} font-bodoni text-black font-normal italic text-sm lg:text-lg md:text-md space-x-5`}>justkimtai@gmail.com</p>
+                                <p className={`${spaceMono.className} text-black font-normal italic text-sm lg:text-lg md:text-md space-x-5`}>justkimtai@gmail.com</p>
                             </div>
                         </Link>
                     </div>
@@ -26,13 +26,13 @@ export const Footer = () => {
                         <Link href="https://facebook.com/justkimtai" target="_blank" rel="noopener noreferrer" className='block'>
                             <div className='flex items-center flex-row space-x-3'>
                                 <Image src="/facebookicon.svg" alt="Facebook icon" width={40} height={40} />
-                                <p className={`${spaceMono.className} font-bodoni text-black font-normal italic text-sm lg:text-lg md:text-md space-x-5`}>Justus Kimtai</p>
+                                <p className={`${spaceMono.className} text-black font-normal italic text-sm lg:text-lg md:text-md space-x-5`}>Justus Kimtai</p>
                             </div>
                         </Link>
                         <Link href="https://twitter.com/justkimtai" target="_blank" rel="noopener noreferrer" className='block'>
                             <div className='flex items-center flex-row space-x-3'>
                                 <Image src="/twittericon.svg" alt="Twitter icon" width={40} height={40} />
-                                <p className={`${spaceMono.className} font-bodoni text-black font-normal italic text-sm lg:text-lg md:text-md space-x-5`}>justkimtai</p>
+                                <p className={`${spaceMono.className} text-black font-normal italic text-sm lg:text-lg md:text-md space-x-5`}>justkimtai</p>
                             </div>
                         </Link>
                     </div>
@@ -40,8 +40,8 @@ export const Footer = () => {
                 <div className='flex flex-col space-y-2'>
                     <Image src="/logo.svg" alt="Logo in the footer section" width={200} height={200} className='self-center' />
                     <div className='self-center'>
-                        <p className={`${spaceMono.className} font-bodoni text-black font-normal text-sm lg:text-lg md:text-md space-x-5`}>Justus Kimtai</p>
-                        <p className={`${spaceMono.className} font-bodoni text-black font-normal text-sm space-x-5`}>2024</p>
+                        <p className={`${spaceMono.className} text-black font-normal text-sm lg:text-lg md:text-md space-x-5`}>Justus Kimtai</p>
+                        <p className={`${spaceMono.className} text-black font-normal text-sm space-x-5`}>2024</p>
                     </div>
                 </div>
             </div>
