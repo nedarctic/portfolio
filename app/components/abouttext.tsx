2fdb8cedd282e91575b72cb5202d5ff1a45d2693
nedarctic <nedarctic@gmail.com>
2025-07-22 10:42:11 +0300

    Centered about text

--- app/components/abouttext.tsx
+++ app/components/abouttext.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/ScrollAnimate.module.css';
 
 export const AboutText = () => {
     return (
-        <p className={`${spaceMono.className} ${styles.scrollanimate} text-white font-normal italic my-2 p-4 text-sm lg:text-2xl md:text-md w-full md:w-1/2`}>
+        <p className={`${spaceMono.className} ${styles.scrollanimate} text-white font-normal italic my-2 p-4 text-sm lg:text-2xl md:text-md w-full`}>
             I am a full-stack web/android developer with over <span className="font-semibold underline">5 years of experience</span> based in Kenya with great expertise in developing user-focused solutions.</p>
     );
 }
